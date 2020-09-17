5efeb16ecea09109a9eb6f6ed085c980bf9526a6
Junaid Malik <hafizjunaidmalik@gmail.com>
2020-09-18 00:21:55 +0300

    Update __init__.py

--- fastonn/__init__.py
+++ fastonn/__init__.py
@@ -1,7 +1,6 @@
 from .OpNetwork import OpNetwork
 from .OpTier import OpTier
 from .OpBlock import OpBlock
-from .SelfONN import SelfONNLayer
 from .trainer import Trainer
 from .osl import *
 from . import utils
